cbf0cfe00eada86bcede9c4b43c31278eb126378
jhj0203 <jhj990203@gmail.com>
2024-03-29 15:35:34 +0900

    현지 - 컨트롤러 대소문자 수정

--- src/main/java/com/surdream/surdream/Controller/SessionController.java
+++ src/main/java/com/surdream/surdream/Controller/SessionController.java
@@ -1,4 +1,4 @@
-package com.surdream.surdream.Controller;
+package com.surdream.surdream.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
