fd085a34f853ebae7db2f4d8011dc9a494d99e3f
kokosang11 <golddust312@gmail.com>
2024-04-02 14:22:51 +0900

    써드림 연결 완료

--- python/fastapiMaker.py
+++ python/fastapiMaker.py
@@ -15,13 +15,13 @@ from langchain_core.output_parsers import StrOutputParser
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-import asyncio
 
 # cors 설정을 위해서 import
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
-# requests를 써서 java로 output 보내기 위해서 import
-import requests
+## os를 이용해서 환경변수 설정해주기!!
 
 app = FastAPI()
 
@@ -34,34 +34,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-class Topic(BaseModel):
+class TopicSection(BaseModel):
     topic: str
+    section: str
 
+# 결과를 저장할 클래스 정의
+class Result(BaseModel):
+    section: str
+    result: str
 
 @app.get("/")
 def root():
     return {"root"}
 
-
-# 각 section별 질의
-section_list = [
-    "제안배경 및 필요성",
-    "개발목표",
-    "개발내용",
-    "기대효과 및 활용방안",
-    "STP전략",
-    "데이터 확보방안",
-]
-
-
 # 사용자가 입력한 topic을 받아오는 엔드포인트
 @app.post("/receive_json")
-async def receive_text(topic: Topic):
-    # 전송된 텍스트를 출력
-    print("Received text:", topic)
+async def receive_text(topic_section:TopicSection):
 
-    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    topic = topic_section.topic
+    section = topic_section.section
+    
+    # 전송된 텍스트를 출력
+    print("Received text:", topic , section)
+
+    llm = ChatOpenAI(model="gpt-4-turbo-preview")
 
     # 출력 구문 분석기(채팅 메시지를 문자열로 변환)
     output_parser = StrOutputParser()
@@ -71,7 +67,7 @@ async def receive_text(topic: Topic):
         [
             (
                 "system",
-                "프로젝트 주제 {input}에 대한 기획서의 {system}부분을 작성해줘",
+                "프로젝트 주제 {topic}에 대한 기획서의 {section}부분을 작성해주고 html에 바로 삽입할거라서 ###나 **같은 기호는 사용하지 말고 \n으로 줄바꿈을 나타내줘"
             )
         ]
     )
@@ -79,21 +75,14 @@ async def receive_text(topic: Topic):
     # Chain으로 결합
     chain = prompt | llm | output_parser
 
-    async def invoke(section, results):
-        result = await chain.ainvoke({"system": [section], "input": topic})
-        results.append({"section": section, "result": result})
-        print(section, result)
+    async def invoke():
+        result = await chain.ainvoke({"section": section, "topic": topic})
+        return result
 
-    async def invoke_parallel(section_list):
-        results = []
-        tasks = [invoke(section, results) for section in section_list]
-        await asyncio.gather(*tasks)
-        return results
+    result = await invoke()
+    print(result)
 
-    results = await invoke_parallel(section_list)
-
-    print(results)
-
+    return JSONResponse(content=jsonable_encoder(result))
 
 # 서버 on(실행되면 아래쪽 코드들이 실행이 안되기때문에 맨 아래 둬야할듯)
 if __name__ == "__main__":
--- src/main/java/com/surdream/surdream/Controller/DataController.java
+++ src/main/java/com/surdream/surdream/Controller/DataController.java
@@ -1,11 +1,8 @@
 package com.surdream.surdream.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.surdream.Model.Data;
