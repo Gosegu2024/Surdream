02912215fde596935f67d9bed89c8d1bdbe2a5d0
kokosang11 <golddust312@gmail.com>
2024-04-09 17:08:51 +0900

    프롬프트 개선, 가독성 개선, 기능 개선

--- python/fastapiMaker.py
+++ python/fastapiMaker.py
@@ -36,7 +36,6 @@ from langchain.agents import AgentExecutor
 
 import haedream as hd
 
-
 app = FastAPI()
 
 # cors 설정(get, post로 설정해주기 위해서 씀)
@@ -119,7 +118,11 @@ async def execute_retrival(topic_section: TopicSection):
 
     async def retriver_rag():
         loader = WebBaseLoader(url)
+        # loader = DirectoryLoader(".", glob="data/*.txt", show_progress=True)
         docs = loader.load()
+
+        print(f"문서의 수: {len(docs)}")
+
         embeddings = OpenAIEmbeddings()
         print("web base loader 실행 완료")
 
@@ -165,6 +168,8 @@ async def execute_retrival(topic_section: TopicSection):
 
         result = await invoke_tavily(agent_executor)
 
+        print(result)
+
         result = result["output"]
 
         # log test(오류남)
@@ -182,12 +187,45 @@ async def execute_retrival(topic_section: TopicSection):
 
 
     async def invoke_tavily(agent_executor):
-        return await agent_executor.ainvoke(
+        if topic == "제안배경 및 필요성":
+            print("제안배경 실행!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            return await agent_executor.ainvoke(
+                {
+                "input": f"""프로젝트 주제 {topic}에 대한 기획서의 {section}부분을 기획서 형식으로 작성해줘. 기획서에 들어가는 문장은 존댓말을 쓰면 안돼. 꼭! 업계 동향도 알려줘!
+                최신 자료를 바탕으로 확실한 근거를 가지고 작성해주고 자료의 출처도 마지막에 적어줘. 출처는 a태그로 감싸주고 파란색으로 보이게 해줘. 결과물에는 개행표시는 쓰지말고 <br>태그를 넣어줘. 
+                br태그의 위치는 소제목 다음에 있으면 좋겠어. 특수문자도 쓰지 마. 특히 ** 이 특수문자는 절대 쓰지마!
+                소제목은 b태그로 감싸줘."""
+                }
+            )
+        else:
+            return await agent_executor.ainvoke(
                 {
                 "input": f"""프로젝트 주제 {topic}에 대한 기획서의 {section}부분을 기획서 형식으로 작성해줘. 기획서에 들어가는 문장은 존댓말을 쓰면 안돼. 
-                최신 자료를 바탕으로 확실한 근거를 가지고 작성해주고 자료의 출처도 마지막에 적어줘. 출처는 a태그로 감싸줘. {section}이 제안배경 및 필요성이라면 {topic}에 대한 업계 동향도 넣어줘. 결과물에는 개행표시는 쓰지말고 <br>태그를 넣어줘. 
-                br태그의 위치는 소제목 다음에 있으면 좋겠고 *표시도 빼주면 좋겠어.
-                소제목은 b태그로 감싸줘."""
+                최신 자료를 바탕으로 확실한 근거를 가지고 작성해주고 자료의 출처도 마지막에 적어줘. 출처는 a태그로 감싸주고 파란색으로 보이게 해줘. 결과물에는 개행표시는 쓰지말고 <br>태그를 넣어줘. 
+                br태그의 위치는 소제목 다음에 있으면 좋겠어. 특수문자도 쓰지 마. 특히 ** 이 특수문자는 절대 쓰지마!
+                소제목은 b태그로 감싸줘.
+                예시를 알려줄게
+                제안 배경
+2023년은 LLM (대형 언어 모델) 분야에서 상당한 성장과 혁신이 이루어진 해였다. AI 기술의 빠른 발전과 함께, 오픈 소스 LLM의 등장은 기술과 상호 작용하는 방식을 재구성하고 있다. 특히, 오픈소스 LLM은 비용 절감, 시스템의 효율성 증대, 그리고 다양한 영역에서의 적용 가능성을 제공하며, 이는 기업과 스타트업에게 새로운 기회를 열어주고 있다.
+
+필요성
+오픈소스 LLM을 활용한 키오스크 사업은 여러 면에서 혁신적이다. 첫째, 사용자 경험을 극대화할 수 있다. AI 기반의 언어 모델을 통해 사용자의 요구를 더 정확하게 파악하고, 이에 맞는 응답을 제공함으로써 사용자 만족도를 높일 수 있다. 둘째, 기업의 운영 효율성을 증대시킬 수 있다. 오픈소스 LLM을 이용하면 개발 비용과 시간을 크게 절약할 수 있으며, 이는 특히 자본이 제한적인 중소기업이나 스타트업에게 큰 이점을 제공한다. 셋째, 키오스크 사업의 경쟁력을 강화할 수 있다. AI 기술을 통해 제공할 수 있는 서비스의 범위가 확장되며, 이는 소비자에게 보다 차별화된 서비스를 제공할 수 있는 기회를 의미한다.
+                
+개발 내용
+본 프로젝트는 LLM을 활용한 키오스크 사업을 목표로 하며, 최신 오픈소스 LLM 모델을 기반으로 사용자 경험을 혁신하고, 기업 운영의 효율성을 극대화할 방안을 모색한다. 이를 위해 다음과 같은 개발 내용을 포함한다.
+
+1. 오픈소스 LLM 모델의 선정 및 적용
+2024년 최고의 오픈소스 LLM 중 하나인 라마 2를 주요 모델로 선정한다. 라마 2는 메타 AI에서 개발한 모델로, 다양한 크기와 용도에 맞춰 최적화된 성능을 제공한다. 이 모델을 키오스크 시스템에 적용하여, 사용자 질문에 대한 정확하고 빠른 응답을 가능하게 한다.
+
+2. 사용자 인터페이스(UI) 개선
+LLM의 자연어 처리 능력을 활용하여, 키오스크의 사용자 인터페이스를 개선한다. 이를 통해 사용자가 자연어로 질문하거나 명령을 할 수 있는 직관적인 UI를 제공하며, 사용자 경험을 대폭 향상시킨다.
+
+3. 맞춤형 서비스 제공
+사용자 데이터 분석을 통해 개인화된 서비스를 제공한다. LLM을 이용하여 사용자의 선호도와 이전 상호작용을 분석, 맞춤형 메뉴 추천이나 프로모션 정보를 제공함으로써 사용자 만족도를 높인다.
+
+4. 효율적인 시스템 운영
+오픈소스 LLM을 활용함으로써 개발 비용과 시간을 절약한다. 또한, 시스템 유지보수 및 업데이트를 용이하게 하여, 운영 효율성을 증대시킨다.
+                """
                 }
             )
     
@@ -201,8 +239,6 @@ async def execute_retrival(topic_section: TopicSection):
         )
 
     result = await retriver_rag()
-
-    print(result)
 
     return JSONResponse(content=jsonable_encoder(result))
 
